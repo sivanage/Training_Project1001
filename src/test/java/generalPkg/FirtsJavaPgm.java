7b6df7f56e807bb580a8b203dccbd5e77e1e39ec
sivanage <sivanage@host.docker.internal>
2022-02-16 15:17:35 -0800

    This is my first commit

---
+++ src/test/java/generalPkg/FirtsJavaPgm.java
@@ -0,0 +1,10 @@
+package generalPkg;
+
+public class FirtsJavaPgm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My First Java Program");
+	}
+
+}
